dba588273907e0dd509e1738131f8de14f4bf93e
Stephen <stephensouness@outlook.com>
2026-01-21 15:16:38 +0000

    configure router and app entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -6,9 +7,11 @@ import SignupPage from "./pages/SignupPage";
 function App() {
   return (
     <div>
-      <Dashboard />
-      <LoginPage />
-      <SignupPage />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
