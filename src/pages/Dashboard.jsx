e8e0d6235acbb8bc1ca9572cdec68a7f03fb20f7
Stephen <stephensouness@outlook.com>
2026-01-21 18:57:42 +0000

    add auth check to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../services/api";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMe();
+
+      if (response.error) {
+        localStorage.removeItem("token");
+        navigate("/login");
+        return;
+      }
+      setUser(response.data.user);
+    })();
+  }, [navigate]);
+
+  if (!user) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Logged in as: {user.email}</p>
+    </div>
+  );
 }
 
 export default Dashboard;
